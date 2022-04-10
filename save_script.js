70668c575004bd5623f3212d6ba3c07c481257a3
Alx-Assis <47927016+Alx-Assis@users.noreply.github.com>
2022-04-10 19:39:39 -0300

    Update save_script.js
    ....

--- save_script.js
+++ save_script.js
@@ -3,19 +3,14 @@ let heroName=document.getElementById('heroName')
 let heroImage=document.getElementById('heroImage')
 let heroClass=document.getElementById('heroClasse')
 
-let hero={
-  heroN:heroName.value,
-  heroI:heroImage.src,
-  heroC:heroClasse.value
-}
-hero=JSON.parse(localStorage.getItem('hero'))||[]
+let hero=JSON.parse(localStorage.getItem('hero'))||[]
 
 let btn=document.getElementById('save')
 btn.addEventListener('click',(e)=>{
 hero.heroN=heroName.value
 hero.heroI=heroImage.src
 hero.heroC=heroClasse.value
-console.table(hero)
+
 
 localStorage.setItem('hero',JSON.stringify(hero))
 alert("Ficha Salva com sucesso")
