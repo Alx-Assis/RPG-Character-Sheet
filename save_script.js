c368a77435f61c2f8dfce47617e73bd785b61e76
Alx-Assis <47927016+Alx-Assis@users.noreply.github.com>
2022-04-10 19:44:43 -0300

    Update save_script.js
    ..

--- save_script.js
+++ save_script.js
@@ -9,7 +9,7 @@ let btn=document.getElementById('save')
 btn.addEventListener('click',(e)=>{
 hero.heroN=heroName.value
 hero.heroI=heroImage.src
-hero.heroC=heroClasse.value
+hero.heroC=heroClass.value
 
 
 localStorage.setItem('hero',JSON.stringify(hero))
