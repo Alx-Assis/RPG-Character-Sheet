ad693b9163b9e80882a0afbdab4ccc28c6773a4e
Alx-Assis <47927016+Alx-Assis@users.noreply.github.com>
2022-04-10 18:48:44 -0300

    Add files via upload
    Upload filé save_script.js

---
+++ save_script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',()=>{
+let heroName=document.getElementById('heroName')
+let heroImage=document.getElementById('heroImage')
+let heroClass=document.getElementById('heroClasse')
+
+let hero={
+  heroN:heroName.value,
+  heroI:heroImage.src,
+  heroC:heroClasse.value
+}
+hero=JSON.parse(localStorage.getItem('hero'))||[]
+
+let btn=document.getElementById('save')
+btn.addEventListener('click',(e)=>{
+hero.heroN=heroName.value
+hero.heroI=heroImage.src
+hero.heroC=heroClasse.value
+console.table(hero)
+
+localStorage.setItem('hero',JSON.stringify(hero))
+})
+
+heroClasse.value=hero.heroC
+heroImage.src=hero.heroI
+heroName.value=hero.heroN
+
+})
