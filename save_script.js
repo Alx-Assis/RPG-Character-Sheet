62d9f0d561f1ef3e5a09c827cce085a42617835e
Alx-Assis <47927016+Alx-Assis@users.noreply.github.com>
2022-04-10 21:05:31 -0300

    Update save_script.js
    i

--- save_script.js
+++ save_script.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded',await ()=>{
+document.addEventListener('DOMContentLoaded',()=>{
 let heroName=document.getElementById('heroName')
 let heroImage=document.getElementById('heroImage')
 let heroClasse=document.getElementById('heroClasse')
 
+
 let hero=JSON.parse(localStorage.getItem('hero'))||[]
 
 let btn=document.getElementById('save')
-btn.addEventListener('click',(e)=>{
+btn.addEventListener('click',()=>{
 hero.heroN=heroName.value
 hero.heroI=heroImage.src
 hero.heroC=heroClasse.value
